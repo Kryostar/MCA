1854f06cbf3444152ea8c4c89eb1d9de28d3b1ec
Kryostar <46088339+Kryostar@users.noreply.github.com>
2022-08-09 13:17:31 +0530

    updated .gitignore and python-eraser files

--- MCA-Sem1/DS_Lab/exe.py
+++ MCA-Sem1/DS_Lab/exe.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-dir_name = "C:/Users/1kryo/Documents/MCA_Sem1/MCA/DS_lab/"
+dir_name = "C:/Users/1kryo/Documents/Github/MCA/MCA-Sem1/DS_lab/"
 test = os.listdir(dir_name)
 
 for item in test:
--- MCA-Sem1/OOPS_lab/class.py
+++ MCA-Sem1/OOPS_lab/class.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Deleting all the .class files in the directory.
-dir_name = "C:/Users/1kryo/Documents/MCA_Sem1/MCA/OOPS_lab/"
+dir_name = "C:/Users/1kryo/Documents/Github/MCA/MCA-Sem1/OOPS_lab/"
 test = os.listdir(dir_name)
 for item in test:
     if item.endswith(".class"):
