0553484260229abb95b21600f6f0dbbd929857c9
Kryostar <46088339+Kryostar@users.noreply.github.com>
2022-11-02 12:38:42 +0530

    multiple

---
+++ MCA-Sem2/Unix/part-b/p2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	int fd1, fd2;
+	char buffer[1024];
+	long int n1;
+
+	if (((fd1 = open(argv[1], O_RDONLY)) == -1) || ((fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0700)) == -1)) {
+		perror("file problem");
+		exit(1);
+	}
+
+	while ((n1 = read(fd1, buffer, 1024)) > 0) {
+		if (write(fd2, buffer, n1) != n1) {
+			perror("writing problem ");
+			exit(3);
+		}
+	}
+	close(fd1);
+	close(fd2);
+}
